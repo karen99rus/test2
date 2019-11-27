3a51f4dc0451fd7434ea40be54622127ebc0f0bc
karen99rus <38454242+karen99rus@users.noreply.github.com>
2019-11-28 00:01:13 +0300

    Updates

---
+++ precache-manifest.1edea76776a6271a4a5353419737241e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5397c62853453e970047f89f9edf27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a099e4bd8e7704f685a",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "d1f5b6b2e301e0519a48",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "2a099e4bd8e7704f685a",
+    "url": "/static/js/2.e9c4bc13.chunk.js"
+  },
+  {
+    "revision": "d1f5b6b2e301e0519a48",
+    "url": "/static/js/main.b874e411.chunk.js"
+  },
+  {
+    "revision": "307391c5dfc150a168f4",
+    "url": "/static/js/runtime-main.69fc0e63.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
